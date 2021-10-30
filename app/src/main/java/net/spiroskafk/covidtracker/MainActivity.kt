801c8766427044610e4d51c9215cc2bf1fad9aa1
Spiros Kafkias <spiroskafk@gmail.com>
2021-10-30 13:52:53 +0300

    CovidTracker App

---
+++ app/src/main/java/net/spiroskafk/covidtracker/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.spiroskafk.covidtracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
